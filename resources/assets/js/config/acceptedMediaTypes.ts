51f8768aedcbee9133f3c38cb09ce5006e925b26
BuildTools <unconfigured@null.spigotmc.org>
2024-02-05 22:24:49 -0500

    test3

--- resources/assets/js/config/acceptedMediaTypes.ts
+++ resources/assets/js/config/acceptedMediaTypes.ts
@@ -4,5 +4,9 @@ export const acceptedMediaTypes = [
   'audio/mpeg',
   'audio/ogg',
   'audio/x-flac',
-  'audio/x-aac'
+  'audio/x-aac',
+  'audio/wav',
+  'audio/x-wav',
+  'audio/x-m4a',
+  'audio/x-mp3',
 ]
